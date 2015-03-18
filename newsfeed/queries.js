f41b1327aa1d6eb4d505c746709542982bb9a6dc
Menna ElSayed <mennat.hafez@gmail.com>
2015-03-18 23:57:47 +0200

    fixed the has relation in the match Story #6

--- newsfeed/queries.js
+++ newsfeed/queries.js
@@ -12,11 +12,11 @@ exports.createResturant  = function (name) {
 
 
     //6-I can like a dish in a specific restaurant.
-    // The function takes an email, Dish name and a restaurant name and match the user and restaurant and the dish.
-    // Then it creates a Relation HAS between dish and restaurant and a LIKED Relation between the user and a dish.
-exports.createrLikeUserDish  = function (UserEmail,DishName,RestaurantName) {
-     db.query("MATCH (user:User {email: {ep}}), (dish:Dish {dish_name: {dnp}}),(r:Restaurant {name:{rp}}) CREATE (user)-[:LIKES_DISH]->(dish)<-[:has]-(r) WITH user,dish MATCH (user)-[x:DISLIKES_DISH]->(dish)<-[:has]-(r) DELETE x", 
-     	params = {ep:UserEmail,dnp:DishName,rp:RestaurantName}, function (err, results) {
+    // The function takes an email and Dish name  and match the user and the dish.
+    // Then it creates a Relation LIKED Relation between the user and a dish.
+exports.createrLikeUserDish  = function (UserEmail,DishName) {
+     db.query("MATCH (user:User {email: {ep}}), (dish:Dish {dish_name: {dnp}}) CREATE (user)-[:LIKES_DISH]->(dish) WITH user,dish MATCH (user)-[x:DISLIKES_DISH]->(dish)", 
+     	params = {ep:UserEmail,dnp:DishName}, function (err, results) {
         if (err) throw err;
         console.log('done');
     });
