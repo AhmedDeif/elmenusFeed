875a0e93cca0d1c0c31c054f8a4c0f56a843b5ec
Menna ElSayed <mennat.hafez@gmail.com>
2015-03-18 23:51:24 +0200

    Method of Story 6 and Documentation

--- newsfeed/queries.js
+++ newsfeed/queries.js
@@ -9,4 +9,17 @@ exports.createResturant  = function (name) {
                 }
         else console.log("Done");
     });
+
+
+    //6-I can like a dish in a specific restaurant.
+    // The function takes an email, Dish name and a restaurant name and match the user and restaurant and the dish.
+    // Then it creates a Relation HAS between dish and restaurant and a LIKED Relation between the user and a dish.
+exports.createrLikeUserDish  = function (UserEmail,DishName,RestaurantName) {
+     db.query("MATCH (user:User {email: {ep}}), (dish:Dish {dish_name: {dnp}}),(r:Restaurant {name:{rp}}) CREATE (user)-[:LIKES_DISH]->(dish)<-[:has]-(r) WITH user,dish MATCH (user)-[x:DISLIKES_DISH]->(dish)<-[:has]-(r) DELETE x", 
+     	params = {ep:UserEmail,dnp:DishName,rp:RestaurantName}, function (err, results) {
+        if (err) throw err;
+        console.log('done');
+    });
+}
+
 }
