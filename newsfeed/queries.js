6837418875579078172754d0dae1dba5622c83ae
Amira ElEwady <amiraelewady@live.com>
2015-03-18 23:19:39 +0200

    Added restaurant review

--- newsfeed/queries.js
+++ newsfeed/queries.js
@@ -1,7 +1,14 @@
 var neo4j = require('neo4j');
 var db = new neo4j.GraphDatabase('http://localhost:7474');
 
-
+exports.createrReviewUserToRestaurant = function (UserEmail,RestaurantName,ReviewTitle,ReviewBody) {
+    db.query("MATCH (n:User { email:{ep} }),(r:Restaurant { name:{rp} })CREATE (n) -[:Review { title:{tp} , body:{bp} }]-> (r) ;", params = {ep:UserEmail,rp:RestaurantName,tp:ReviewTitle,bp:ReviewBody}, function (err, results) {
+        if (err){  console.log('Error');
+                 throw err;
+                }
+        else console.log("Done");
+    });
+}
 exports.createResturant  = function (name) {
     db.query("CREATE (:Restaurant { name:{np} })", params = {np:name}, function (err, results) {
         if (err){  console.log('Error');
