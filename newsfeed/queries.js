beb951f7fd1a1cd409bd1cca91112316a43de5d7
Doola <ahmed.adel.abodeif@gmail.com>
2015-03-18 22:58:01 +0200

    Queries file
    Adding the javascript file that will contain all functions that connect
    to the database and interact with it using all queries.

---
+++ newsfeed/queries.js
@@ -0,0 +1,12 @@
+var neo4j = require('neo4j');
+var db = new neo4j.GraphDatabase('http://localhost:7474');
+
+
+exports.createResturant  = function (name) {
+    db.query("CREATE (:Restaurant { name:{np} })", params = {np:name}, function (err, results) {
+        if (err){  console.log('Error');
+                 throw err;
+                }
+        else console.log("Done");
+    });
+}
