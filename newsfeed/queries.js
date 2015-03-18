8b783f90097447b80ae7150a222fa97186ded0ac
Rania Abdel Monem <raniimenem@gmail.com>
2015-03-18 23:19:20 +0200

    added "As a user, I can sign up

--- newsfeed/queries.js
+++ newsfeed/queries.js
@@ -2,6 +2,14 @@ var neo4j = require('neo4j');
 var db = new neo4j.GraphDatabase('http://localhost:7474');
 
 
+exports.createUser  = function (email) {
+    db.query("CREATE (:User { email:{ep} })", params = {ep:email}, function (err, results) {
+        if (err){  console.error('Error');
+                 throw err;
+                }
+        else console.log("Done");
+    });
+}
 exports.createResturant  = function (name) {
     db.query("CREATE (:Restaurant { name:{np} })", params = {np:name}, function (err, results) {
         if (err){  console.log('Error');
