14f8d4c55b8e496dea1e779b11e28b079164724f
Amira ElEwady <amiraelewady@live.com>
2015-03-18 23:33:22 +0200

    Added line

--- newsfeed/queries.js
+++ newsfeed/queries.js
@@ -1,6 +1,7 @@
 var neo4j = require('neo4j');
 var db = new neo4j.GraphDatabase('http://localhost:7474');
 
+
 exports.createrReviewUserToRestaurant = function (UserEmail,RestaurantName,ReviewTitle,ReviewBody) {
     db.query("MATCH (n:User { email:{ep} }),(r:Restaurant { name:{rp} })CREATE (n) -[:Review { title:{tp} , body:{bp} }]-> (r) ;", params = {ep:UserEmail,rp:RestaurantName,tp:ReviewTitle,bp:ReviewBody}, function (err, results) {
         if (err){  console.log('Error');
