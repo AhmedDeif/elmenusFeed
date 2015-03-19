defb50c6879d320067c6129ae3e6676ef15aa20b
Amira ElEwady <amiraelewady@live.com>
2015-03-19 10:10:12 +0200

    Added description for US 5

--- newsfeed/queries.js
+++ newsfeed/queries.js
@@ -1,7 +1,10 @@
 var neo4j = require('neo4j');
 var db = new neo4j.GraphDatabase('http://localhost:7474');
 
-
+//(S5) The function takes the following parameters:
+//UserEmail, the restaurant name, the review title and the body of the review
+//then it matches the user with the restaurant
+//and adds the review to this restaurant
 exports.createrReviewUserToRestaurant = function (UserEmail,RestaurantName,ReviewTitle,ReviewBody) {
     db.query("MATCH (n:User { email:{ep} }),(r:Restaurant { name:{rp} })CREATE (n) -[:Review { title:{tp} , body:{bp} }]-> (r) ;", params = {ep:UserEmail,rp:RestaurantName,tp:ReviewTitle,bp:ReviewBody}, function (err, results) {
         if (err){  console.log('Error');
