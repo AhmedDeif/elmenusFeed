9b37cd2154739bd333610412a09e878d95a26b6a
Rania Abdel Monem <raniimenem@gmail.com>
2015-03-19 07:01:54 +0200

    added description for US3

--- newsfeed/queries.js
+++ newsfeed/queries.js
@@ -1,7 +1,9 @@
 var neo4j = require('neo4j');
 var db = new neo4j.GraphDatabase('http://localhost:7474');
 
-
+//(S3) I can sign up.
+//The function takes the email of the user as an input.
+//and it creates a new user.
 exports.createUser  = function (email) {
     db.query("CREATE (:User { email:{ep} })", params = {ep:email}, function (err, results) {
         if (err){  console.error('Error');
