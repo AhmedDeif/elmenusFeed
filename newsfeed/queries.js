5c6cec9262e0a1524b0fe4f92181df8c5eefcd3f
elhagin <omar.elhagin@live.com>
2015-03-20 13:54:01 +0200

    enhanced documentation

--- newsfeed/queries.js
+++ newsfeed/queries.js
@@ -3,7 +3,8 @@ var db = new neo4j.GraphDatabase('http://localhost:7474');
 
 
 exports.createResturant  = function (name) {
-    db.query("CREATE (:Restaurant { name:{np} })", params = {np:name}, function (err, results) {
+    db.query("CREATE (:Restaurant { name:{np} })",
+     params = {np:name}, function (err, results) {
         if (err){  console.log('Error');
                  throw err;
                 }
@@ -13,15 +14,21 @@ exports.createResturant  = function (name) {
 
 //2-I can add a dish to the resturant TESTED
 exports.createDish  = function (name) {
-    db.query("CREATE (:Dish { dish_name:{np} })", params = {np:name}, function (err, results) {
+    db.query("CREATE (:Dish { dish_name:{np} })",
+     params = {np:name}, function (err, results) {
         if (err){  console.error('Error');
                  throw err;
                 }
         else console.log("Done");
     });
 }
-exports.createrAddDishToRestaurant  = function (dish,restaurant) {
-    db.query("MATCH (d:Dish),(r:Restaurant) WHERE d.dish_name={dp} AND r.name ={rp} CREATE (r)-[rl:Has]->(d)", params = {dp:dish,rp:restaurant}, function (err, results) {
+exports.addDishToRestaurant  =
+ function (dish,restaurant) {
+    db.query("MATCH (d:Dish),(r:Restaurant) WHERE 
+        d.dish_name={dp} AND r.name ={rp} CREATE 
+        (r)-[rl:HAS_DISH]->(d)", params = 
+        {dp:dish,rp:restaurant},
+         function (err, results) {
         if (err){  console.error('Error');
                  throw err;
                 }
@@ -29,9 +36,24 @@ exports.createrAddDishToRestaurant  = function (dish,restaurant) {
     });
 }
 
-//18-I can follow another user. TESTED
-exports.createrFollowUserUser  = function (FollowerEmail,FolloweeEmail) {
-    db.query("MATCH (d:User),(r:User)  WHERE d.email={e1p} AND r.email = {e2p} AND d.email <> r.email   CREATE (d)-[f:Follows]->(r)", params = {e1p:FollowerEmail,e2p:FolloweeEmail}, function (err, results) {
+/* Sprint #-0-US-18
+    createFollowUser(FollowerEmail, FolloweeEmail):
+    This function takes as an input the email of 
+    the user that is requesting to follow another
+    user, and the email of the other user that is
+    being requested to be followed, then checks
+    that these two emails are not the same (a user
+    cannot follow his/herself). Finally, it creates
+    the corresponding FOLLOWS relationship between
+    these two users.
+*/
+exports.createFollowUser = function (FollowerEmail,FolloweeEmail) {
+    db.query("MATCH (d:User),(r:User)  WHERE 
+        d.email={e1p} AND r.email = {e2p} AND 
+        d.email <> r.email   CREATE (d)-[f:FOLLOWS]
+        ->(r)", params = {e1p:FollowerEmail
+            ,e2p:FolloweeEmail}
+            , function (err, results) {
         if (err){  console.log('Error');
                  throw err;
                 }
