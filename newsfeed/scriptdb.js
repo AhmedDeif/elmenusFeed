d5a9ee572bf83fefb2befb3d2ef69d535ff25254
kareem <al3ameeeel94@gmail.com>
2015-04-04 18:34:03 +0200

    Import relation like dish US 17
    #77

--- newsfeed/scriptdb.js
+++ newsfeed/scriptdb.js
@@ -57,3 +57,33 @@ function reviewsOfRestaurants(){
   }
 });
 }
+
+//----------------------------------------------------------------------------------------
+// create like
+
+
+function likeDish(){
+if (fs.existsSync('C:/tmp/createLikeUserDishRestaurant.csv')) {
+    fs.unlinkSync('C:/tmp/createLikeUserDishRestaurant.csv');
+}
+  connection.query("select \'User\',\'Dish\',\'Restaurant\' UNION select u.email As User,d.name_en AS Dish,r.name_en AS Restaurant from items d,items_likes l , restaurants r , users u,menus m where  d.id = l.item_id and u.id = l.user_id and l.user_id>=0 and d.menu_id=m.id and m.restaurant_id=r.id and l.is_liked=1 INTO OUTFILE \'C:/tmp/createLikeUserDishRestaurant.csv\' FIELDS TERMINATED BY \',\' ENCLOSED BY \'\"\' LINES TERMINATED BY \'\n\';"
+  , function(err, rows, fields) {
+  if (!err){
+    setTimeout(function(){
+      db.query("USING PERIODIC COMMIT LOAD CSV WITH HEADERS FROM \"file:///C:/tmp/createLikeUserDishRestaurant.csv\" AS row match (Restaurant {Name:row.Restaurant})-[:Has]->(d:Dish {Name:row.Dish}) match (u:User {Email:row.User}) MERGE (u) -[:Like]-> (d) return d limit 1;", params = {}, function (err, results) {
+        if (err){  
+                    throw err;
+                }
+        else {
+          console.log('User-Dish-Like Done');
+          dislikeDish();
+        }
+    });
+  },400);
+          
+}
+  else{
+    throw err;
+  }
+});
+}
