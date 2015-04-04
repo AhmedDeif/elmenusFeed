a63ce1baea12d4c7aceb27dd47076f640d2f81d5
kareem <al3ameeeel94@gmail.com>
2015-04-04 18:31:55 +0200

    imported Restaurant Reviews US 14
    #76

--- newsfeed/scriptdb.js
+++ newsfeed/scriptdb.js
@@ -26,3 +26,34 @@ function users(){
   }
 });
 }
+
+//----------------------------------------------------------------------------------------
+// create review
+
+function reviewsOfRestaurants(){
+  if (fs.existsSync('C:/tmp/createreviewUserRestaurant.csv')) {
+    fs.unlinkSync('C:/tmp/createreviewUserRestaurant.csv');
+}
+  connection.query("select \'Restaurant\',\'User\',\'title\',\'body\' UNION select r.name_en AS Restaurant,u.email AS User,v.subject AS title,v.body AS body from restaurants r ,users u,reviews v where v.user_id = u.id and r.id = v.restaurant_id INTO OUTFILE \'C:/tmp/createreviewUserRestaurant.csv\' FIELDS TERMINATED BY \',\' ENCLOSED BY \'\"\' LINES TERMINATED BY \'\n\';"
+  , function(err, rows, fields) {
+  if (!err){
+    setTimeout(function(){
+      db.query("USING PERIODIC COMMIT LOAD CSV WITH HEADERS FROM \"file:///C:/tmp/createreviewUserRestaurant.csv\" AS row match (r:Restaurant {Name:row.Restaurant}) match (u:User {Email:row.User}) MERGE (u) -[:Review { title:row.title, body:row.body }]-> (r) return r limit 1;", params = {}, function (err, results) {
+        if (err){  
+                  throw err;
+                }
+        else {
+          console.log('Reviews-Restaurants Done');
+          addToFavourite();
+        }
+    });
+  },400);
+          
+      
+
+}
+  else{
+    throw err;
+  }
+});
+}
