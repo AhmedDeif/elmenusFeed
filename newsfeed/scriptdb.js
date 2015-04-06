f68e843b51bf0b6bc24c77e0a9b65b6de9e89e62
Hossam Hazem <hossamhazem94@gmail.com>
2015-04-06 23:59:58 +0200

    adding documentation

--- newsfeed/scriptdb.js
+++ newsfeed/scriptdb.js
@@ -16,7 +16,9 @@ uniqueUser();
 
 //---------------------------------------------------------------------------------
 //Create constrains
-
+/*
+creating constraints in Neo4j database to speed up query processing 
+*/
 function uniqueUser(){
   db.query("CREATE CONSTRAINT ON (x:User) ASSERT x.id IS UNIQUE;", params = {}, function (err, results) {
         if (err){  
@@ -43,6 +45,10 @@ function uniqueRestaurant(){
 
 //------------------------------------------------------------------------------------
 //create indices
+/*
+creating indices in Neo4j database to speed up query processing
+and to speed up searching for records 
+*/
 function indexDishRes(){
   db.query("create index on :Dish(Restaurant);", params = {}, function (err, results) {
         if (err){  
@@ -93,8 +99,8 @@ function indexDishName(){
 //-----------------------------------------------------------------------------
 // Create users
 
-
 function users(){
+    
   if (fs.existsSync('C:/tmp/createusers.csv')) {
     fs.unlinkSync('C:/tmp/createusers.csv');
 }
@@ -123,18 +129,29 @@ function users(){
 //Create restaurants
 
 function restaurants(){
+    //check if the csv file exists if it exists delete
+    //it as it isnt deleted mysql will crash
   if (fs.existsSync('C:/tmp/allRestaurants.csv')) {
     fs.unlinkSync('C:/tmp/allRestaurants.csv');
   }
+    //selecting restaurant name from mysql database
+    //from Restaurant table and putting it in a csv file
+    //that each record of restaurant name  in one line 
+    //and creating headers of Restaurant
   connection.query("select \'Restaurant\' UNION SELECT name_en AS Restaurant FROM restaurants INTO OUTFILE \'/tmp/allRestaurants.csv\' FIELDS TERMINATED BY \',\' ENCLOSED BY \'\"\' LINES TERMINATED BY \'\n\';"
   , function(err, rows, fields) {
   if (!err){
+    //creating restaurants with restaurant name in Neo4j database
+    //by importing each record from CSV and saving each record
+    //one by one in 'row' then extracting information from 
+    //it by using headers (row.Restaurant)
           db.query("USING PERIODIC COMMIT LOAD CSV WITH HEADERS FROM \"file:///C:/tmp/allRestaurants.csv\" AS row MERGE (R:Restaurant {Name: row.Restaurant}) return R.Name limit 1;", params = {}, function (err, results) {
         if (err){  
                   throw err;
                 }
         else {
           console.log('Restaurants Done');
+        //calling next method to make code run synchronous
           dishes();
         }
     });
@@ -214,19 +231,33 @@ function reviewsOfRestaurants(){
 
 
 function dishesInRestaurants(){
+    //check if the csv file exists if it exists delete
+    //it as it isnt deleted mysql will crash
   if (fs.existsSync('C:/tmp/dishesRestaurant.csv')) {
     fs.unlinkSync('C:/tmp/dishesRestaurant.csv');
 }
+    //selecting restaurant name and dish name and linking them
+    //by using inner joins between restaurants,menus and items
+    // then creating headers of (Restaurant and Dish) and
+    //putting it in a csv file that each 
+    // record of restaurant name and dish name  in one line
   connection.query("select \'Restaurant\',\'Dish\' UNION select  r.name_en As Restaurant,d.name_en As Dish from restaurants r, items d,menus m where m.restaurant_id = r.id and d.menu_id=m.id INTO OUTFILE \'C:/tmp/dishesRestaurant.csv\' FIELDS TERMINATED BY \',\' ENCLOSED BY \'\"\' LINES TERMINATED BY \'\n\';"
   , function(err, rows, fields) {
   if (!err){
     setTimeout(function(){
+    //creating relations between restaurant and dishes [:Has] in Neo4j database
+    //by matching restaurants' name and dishes name and restaurant name
+    //(to gurantee that dishes are unique to restaurants) then 
+    //importing each record from CSV and saving each record
+    //one by one in 'row' then extracting information from 
+    //it by using headers (row.Restaurant and row.Dish)
       db.query("USING PERIODIC COMMIT LOAD CSV WITH HEADERS FROM \"file:///C:/tmp/dishesRestaurant.csv\" AS row match (r:Restaurant {Name:row.Restaurant}) match (d:Dish {Name:row.Dish,Restaurant:row.Restaurant}) MERGE (r)-[:Has]->(d) return r limit 1;", params = {}, function (err, results) {
         if (err){  
                     throw err;
                 }
         else {
           console.log('Dishes-Restaurants Done');
+        //calling next method to make code run synchronous    
           reviewsOfRestaurants();
         }
     });
@@ -307,19 +338,34 @@ if (fs.existsSync('C:/tmp/createLikeUserDishRestaurant.csv')) {
 // create dislike dish
 
 function dislikeDish(){
+    //check if the csv file exists if it exists delete
+    //it as it isnt deleted mysql will crash
 if (fs.existsSync('C:/tmp/createDislikeUserDishRestaurant.csv')) {
     fs.unlinkSync('C:/tmp/createDislikeUserDishRestaurant.csv');
 }
+    //selecting restaurant name and dish name and user name
+    //then linking them by using inner joins
+    // between restaurants,menus, users, items and item likes
+    // and getting likes where items_likes.is_liked=0
+    // then creating headers of (User,Dish and Restaurant) and
+    //putting it in a csv file that each 
+    // record of user,restaurant name and dish name  in one line
   connection.query("select \'User\',\'Dish\',\'Restaurant\' UNION select u.email As User,d.name_en AS Dish,r.name_en AS Restaurant from items d,items_likes l , restaurants r , users u,menus m where  d.id = l.item_id and u.id = l.user_id and l.user_id>=0 and d.menu_id=m.id and m.restaurant_id=r.id and l.is_liked=0 INTO OUTFILE \'C:/tmp/createDislikeUserDishRestaurant.csv\' FIELDS TERMINATED BY \',\' ENCLOSED BY \'\"\' LINES TERMINATED BY \'\n\';"
   , function(err, rows, fields) {
   if (!err){
     setTimeout(function(){
+    //creating relations between users and dishes of [:Dislike] in Neo4j database
+    //by matching restaurants' name and dishes' name and users' name
+    //then importing each record from CSV and saving each record
+    //one by one in 'row' then extracting information from 
+    //it by using headers (row.Restaurant and row.Dish)
       db.query("USING PERIODIC COMMIT LOAD CSV WITH HEADERS FROM \"file:///C:/tmp/createDislikeUserDishRestaurant.csv\" AS row match (Restaurant {Name:row.Restaurant})-[:Has]->(d:Dish {Name:row.Dish}) match (u:User {Email:row.User}) MERGE (u) -[:Dislike]-> (d) return d limit 1;", params = {}, function (err, results) {
         if (err){  
                     throw err;
                 }
         else {
           console.log('User-Dish-DISLike Done');
+            //calling next method to make code run synchronous
           followUser();
         }
     });
