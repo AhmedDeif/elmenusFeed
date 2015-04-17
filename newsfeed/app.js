2437325c0670097b1d331b3c52b1d99b40ff1db3
Doola <ahmed.adel.abodeif@gmail.com>
2015-04-17 20:26:55 +0200

    fixing routes

--- newsfeed/app.js
+++ newsfeed/app.js
@@ -41,31 +41,33 @@ app.post('/relations', function(req, res) {
   var relation = req.param("rels");
   res.redirect('/Get_relation_info/' + relation);
 });
+
 app.get('/add_dish', routes.addDish);
+
 app.post('/new_dish', function(req, res) {
 	var dishName = req.body.dishName;
 	var restaurant = req.param("rests");
   queries.createDishAndRestaurant(dishName, restaurant);
 	res.redirect('/add_dish');
-});
+})
 app.get('/costChange', routes.costChange);
 app.post('/costChange', function(req, res) {
   var cost = req.body.cost;
   var relation = req.body.relation;
   queries.changeRelationCost(relation, cost);
   res.redirect('/Relations');
-});
+})
 app.get('/Relations', routes.Relations);
 app.post('/Relations', function(req, res) {
   var relation = req.param("select");
   res.redirect('/costChange');
-});
+})
 app.get('/signup', routes.signUp);
 app.post('/sign_up', function(req, res) {
   var email = req.body.email;
   queries.createUser(email);
   res.redirect('/signup');
-});
+})
 
 app.get('/add_review', routes.newReview);
 app.post('/new_review', function(req, res) {
@@ -79,7 +81,7 @@ app.post('/new_review', function(req, res) {
 
   queries.createrReviewUserToRestaurant(Email,restaurantName,reviewTitle,reviewBody);
   res.redirect('/add_review');
-});
+})
 
-app.listen(3000);
-console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
+ app.listen(3000);
+ console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
