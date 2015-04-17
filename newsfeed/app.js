e65488d1678f69d6d6d0195d7dd58a9f0c3934c7
elhagin <omar.elhagin@live.com>
2015-04-17 14:16:33 +0200

    Issue #140: added GET function in app.js

--- newsfeed/app.js
+++ newsfeed/app.js
@@ -94,6 +94,8 @@ app.post('/log_in', function(req, res) {
   });
 });
 
+app.get('/newsfeed/:email', routes.showNewsfeed);
+
  app.listen(3000);
  console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
 
