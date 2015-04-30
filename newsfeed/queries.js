5e7e98a99ad18af153baf7fbe5cdfa5938f5cd8b
Hossam Hazem <hossamhazem94@gmail.com>
2015-04-30 21:10:23 +0200

    increase score on yum and decrease score on yuck between user and cuisine is added
    #180

--- newsfeed/queries.js
+++ newsfeed/queries.js
@@ -302,6 +302,7 @@ exports.createFollowUser = function(FollowerEmail, FolloweeEmail) {
 var UserCommonYumsUserQuery = "MATCH (user1 {email:{ep1}})-[:YUM_YUCK {value: TRUE}]->(photo:Photo)<- [y:YUM_YUCK {value: TRUE}]-"+
                             "(user2 {email:{ep2}}),  (user1)-[f:FOLLOWS]-> (user2) set f.totalScore = f.totalScore+ y.score "+
                             "set f.commonYumYuck = f.commonYumYuck + 1;";
+
 exports.UserCommonYumsUser  = function (UserEmail, UserEmailFollowed) {
     db.query(UserCommonYumsUserQuery, 
         params = {
@@ -448,6 +449,7 @@ exports.visitFollowUser = function(FollowerEmail, FolloweeEmail) {
      and replaced by a yum.
 */
 exports.UserAddPhotoYumsQuery = "MATCH (user:User {email: {ep}}), (photo:Photo {url: {url}}) CREATE (user)-[:YUM_YUCK {value: TRUE, score: 3}]->(photo) WITH user,photo MATCH (user)-[x:YUM_YUCK {value: FALSE, score: 3}]->(photo) Delete x;";
+exports.UserAddPhotoYumsScore="MATCH (n:User { email:{ep} })-[ts:totalscore]-(c:Cuisine)<-[:HasCuisine]-(r:Restaurant)<-[:IN]-(p:Photo{url: {url}}),(g:Scores) set ts.score = ts.score+g.yum_yuckScore"
 exports.UserAddPhotoYums = function(UserEmail, PhotoURL) {
     db.query(exports.UserAddPhotoYumsQuery, params = {
         ep: UserEmail,
@@ -456,7 +458,18 @@ exports.UserAddPhotoYums = function(UserEmail, PhotoURL) {
         if (err) {
             throw err;
         }
-        console.log('done');
+        else{
+                db.query(exports.UserAddPhotoYumsScore, params = {
+                ep: UserEmail,
+                url: PhotoURL
+                }, function(err, results) {
+                if (err) {
+                    throw err;
+                }
+                console.log('done');
+                });
+        }
+        
     });
 }
 /*   Sprint #-1-US-4
@@ -489,6 +502,7 @@ exports.UserDeletePhotoYum = function(UserEmail, PhotoURL) {
      and replaced by a yuck.
 */
 exports.UserAddPhotoYucksQuery = "MATCH (user:User {email: {ep}}), (photo:Photo {url: {url}}) CREATE (user)-[:YUM_YUCK {value: FALSE, score: 3}]->(photo) WITH user,photo MATCH (user)-[x:YUM_YUCK {value: TRUE, score: 3}]->(photo) Delete x;";
+exports.UserAddPhotoYucksScore="MATCH (n:User { email:{ep} })-[ts:totalscore]-(c:Cuisine)<-[:HasCuisine]-(r:Restaurant)<-[:IN]-(p:Photo{url: {url}}),(g:Scores) set ts.score = ts.score-g.yum_yuckScore"
 exports.UserAddPhotoYucks = function(UserEmail, PhotoURL) {
     db.query(exports.UserAddPhotoYucksQuery, params = {
         ep: UserEmail,
@@ -497,7 +511,17 @@ exports.UserAddPhotoYucks = function(UserEmail, PhotoURL) {
         if (err){
             throw err;
         }
-        console.log('done');
+       else{
+                db.query(exports.UserAddPhotoYucksScore, params = {
+                ep: UserEmail,
+                url: PhotoURL
+                }, function(err, results) {
+                if (err) {
+                    throw err;
+                }
+                console.log('done');
+                });
+        }
     });
 }
 /* 	 User Story S11
